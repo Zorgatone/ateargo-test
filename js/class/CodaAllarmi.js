08264113dab84162c2a3e3e007306de903dc29cb
Tommaso Ricci <t.ricci@comunico.info>
2015-04-27 11:52:11 +0200

    aggiunta funzione incompleta per ordinamento gruppi supercodice

--- js/class/CodaAllarmi.js
+++ js/class/CodaAllarmi.js
@@ -34,59 +34,63 @@
     };
 
     supercodiceIndex = function supercodiceIndex(parentList, x) {
-        var a, low, high, len, mid, date1, date2, found;
-
-        a = parentList;
-        len = a.length;
-        low = 0;
-        mid = 0;
-        high = (len > 0) ? len - 1 : 0;
-        found = -1;
-
-        if(len === 0) {
-            return 0;
-        }
-
-        while (low <= high) {
-            mid = Math.floor((low + high) / 2);
-            date1 = x.toDate();
-            date2 = a[mid].toDate();
-
-            if(x.supercodice.toUpperCase() === a[mid].supercodice.toUpperCase()) {
-                found = mid;
-                break;
-            } else if (x.supercodice.toUpperCase() > a[mid].supercodice.toUpperCase()) {
-                high = mid - 1;
-            } else if (x.supercodice.toUpperCase() < a[mid].supercodice.toUpperCase()) {
-                low = mid + 1;
-            }
-        }
-
-        if (found >= 0) {
-            if(a[found].toDate() >= x.toDate()) {
-                return -1; // Do not insert
-            } else {
-                a.remove(found);
-            }
-        }
-
-        low = 0;
-        mid = 0;
-        len = a.length;
-        high = (len > 0) ? len - 1 : 0;
-
-        while (low <= high) {
-            mid = Math.floor((low + high) / 2);
-
-            if (x.toDate() >= a[mid].toDate()) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
+        //@TODO: usare lista di date contenenti i supercodici di quella data (ultime 24h)
     };
+
+    //supercodiceIndex = function supercodiceIndex(parentList, x) {
+    //    var a, low, high, len, mid, date1, date2, found;
+    //
+    //    a = parentList;
+    //    len = a.length;
+    //    low = 0;
+    //    mid = 0;
+    //    high = (len > 0) ? len - 1 : 0;
+    //    found = -1;
+    //
+    //    if(len === 0) {
+    //        return 0;
+    //    }
+    //
+    //    while (low <= high) {
+    //        mid = Math.floor((low + high) / 2);
+    //        date1 = x.toDate();
+    //        date2 = a[mid].toDate();
+    //
+    //        if(x.supercodice.toUpperCase() === a[mid].supercodice.toUpperCase()) {
+    //            found = mid;
+    //            break;
+    //        } else if (x.supercodice.toUpperCase() > a[mid].supercodice.toUpperCase()) {
+    //            high = mid - 1;
+    //        } else if (x.supercodice.toUpperCase() < a[mid].supercodice.toUpperCase()) {
+    //            low = mid + 1;
+    //        }
+    //    }
+    //
+    //    if (found >= 0) {
+    //        if(a[found].toDate() >= x.toDate()) {
+    //            return -1; // Do not insert
+    //        } else {
+    //            a.remove(found);
+    //        }
+    //    }
+    //
+    //    low = 0;
+    //    mid = 0;
+    //    len = a.length;
+    //    high = (len > 0) ? len - 1 : 0;
+    //
+    //    while (low <= high) {
+    //        mid = Math.floor((low + high) / 2);
+    //
+    //        if (x.toDate() >= a[mid].toDate()) {
+    //            high = mid - 1;
+    //        } else {
+    //            low = mid + 1;
+    //        }
+    //    }
+    //
+    //    return low;
+    //};
 
     codiceIndex = function codiceIndex(a, x) {
         var low, high, len, mid;
